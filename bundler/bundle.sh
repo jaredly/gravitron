bedd98a100c4b4074e86d2f11cf0a784f05a4224
Jared Forsyth <jared@jaredforsyth.com>
2017-12-02 10:03:02 -0700

    automate bundling

--- bundler/bundle.sh
+++ bundler/bundle.sh
@@ -1,4 +1,9 @@
 rm -rf Gravitron.app
 ./macapp.sh Gravitron icon.png
-cp ../lib/bs/native/prod.native Gravitron.app/Contents/MacOS/Gravitron
+DEST=Gravitron.app/Contents/MacOS
+cp ../lib/bs/native/prod.native $DEST/Gravitron
+SDL_PATH=`otool -L $DEST/Gravitron | grep libSDL | sed -e 's/(.*//' | sed -e 's/[[:space:]]//g'`
+install_name_tool -change $SDL_PATH @executable_path/libSDL2-2.0.0.dylib $DEST/Gravitron
+cp $SDL_PATH $DEST/libSDL2-2.0.0.dylib
 cp -r ../assets Gravitron.app/Contents/MacOS/
+zip -r Gravitron.zip Gravitron.app
