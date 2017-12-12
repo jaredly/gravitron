129873e5f484c44309e19531fcc9c36e6613f28c
Jared Forsyth <jared@jaredforsyth.com>
2017-12-12 15:31:43 -0700

    making reasongl an android library ish

--- android/app/src/main/java/com/example/hellolibs/MainActivity.java
+++ android/app/src/main/java/com/example/hellolibs/MainActivity.java
@@ -1,29 +1,7 @@
 package com.example.hellolibs;
 
-import com.jaredforsyth.reasongl.ReasonGLView;
+import com.jaredforsyth.reasongl.ReasonglActivity;
 
-import android.app.Activity;
-import android.os.Bundle;
-import android.view.View;
-
-public class MainActivity extends Activity {
-    ReasonGLView glView;
-    @Override
-    public void onBackPressed() {
-        glView.handleBackPressed(new Runnable() {
-            @Override
-            public void run() {
-                MainActivity.super.onBackPressed();
-            }
-        });
-    }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);
-//        getWindowManager().getDefaultDisplay().
-        glView = new ReasonGLView(this);
-        setContentView(glView);
-    }
+public class MainActivity extends ReasonglActivity {
+    // Nothing to override at the moment.
 }
