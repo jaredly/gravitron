61a3424ae1968168c84405874bf3ba70f7d9c327
Jared Forsyth <jared@jaredforsyth.com>
2018-01-03 23:30:11 -0700

    umm moving lots of things around

--- assets/Reprocessing.h
+++ assets/Reprocessing.h
@@ -9,7 +9,7 @@
 #import <UIKit/UIKit.h>
 #import <GLKit/GLKit.h>
 
-@interface GameViewController : GLKViewController
+@interface ReprocessingViewController : GLKViewController
 
 @property (strong, nonatomic) EAGLContext *context;
 
--- ios/Gravitron/AppDelegate.m
+++ ios/Gravitron/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "Reprocessing.h"
 
 @interface AppDelegate ()
 
@@ -17,6 +18,10 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    UIViewController *viewController = [[ReprocessingViewController alloc] init];
+    self.window.rootViewController = viewController;
+    [self.window makeKeyAndVisible];
     return YES;
 }
 
